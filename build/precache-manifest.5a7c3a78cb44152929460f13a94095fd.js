e011701cade8760c6095f641fa94326fe8edb39e
halynaburdiian <verdamtion@gmail.com>
2019-09-05 13:18:51 +0200

    Commit build folder

---
+++ build/precache-manifest.5a7c3a78cb44152929460f13a94095fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4272fb96b2c92bf21ac9ce035b00af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ab75832e441ac7e0a3",
+    "url": "/static/css/main.b25f61b6.chunk.css"
+  },
+  {
+    "revision": "777de50e192073325908",
+    "url": "/static/js/2.428aad0b.chunk.js"
+  },
+  {
+    "revision": "08ab75832e441ac7e0a3",
+    "url": "/static/js/main.f4e9e290.chunk.js"
+  },
+  {
+    "revision": "16e7c43e40469041143b",
+    "url": "/static/js/runtime~main.f7814719.js"
+  }
+]);
